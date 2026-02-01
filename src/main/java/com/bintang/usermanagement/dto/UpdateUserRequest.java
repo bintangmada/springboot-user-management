e259d3222edbfa4e460b746870ed0259cf64c2b2
bintangmada <bintang.mada.suharsono@bni.co.id>
2026-02-02 00:30:59 +0700

    feat: add update user request dto

---
+++ src/main/java/com/bintang/usermanagement/dto/UpdateUserRequest.java
@@ -0,0 +1,16 @@
+package com.bintang.usermanagement.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class UpdateUserRequest {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+}
