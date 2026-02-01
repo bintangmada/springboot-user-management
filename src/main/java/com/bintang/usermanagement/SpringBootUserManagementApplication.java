1a1481e84579a8c0293bb5e56e0f810e3c60f042
bintangmada <bintang.mada.suharsono@bni.co.id>
2026-02-01 20:56:45 +0700

    chore: add application startup log

--- src/main/java/com/bintang/usermanagement/SpringBootUserManagementApplication.java
+++ src/main/java/com/bintang/usermanagement/SpringBootUserManagementApplication.java
@@ -1,14 +1,18 @@
 package com.bintang.usermanagement;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpringBootUserManagementApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(SpringBootUserManagementApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootUserManagementApplication.class, args);
-		System.out.println("\nSERVER IS RUNNING");
+		log.info("SERVER IS RUNNING");
 	}
 
 }
