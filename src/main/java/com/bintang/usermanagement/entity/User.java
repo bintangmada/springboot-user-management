6b8eecead8fe24a7fada36b80a1a61185dd0e2d4
bintangmada <bintang.mada.suharsono@bni.co.id>
2026-02-01 20:37:42 +0700

    feat: add user entity

---
+++ src/main/java/com/bintang/usermanagement/entity/User.java
@@ -0,0 +1,27 @@
+package com.bintang.usermanagement.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+}
