667e5be208491a0aea91d4f04eacfd393621d7f5
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-02 10:09:18 +0700

    refactor: resource not found exception

--- src/main/java/com/bintang/usermanagement/exception/ResourceNotFoundException.java
+++ src/main/java/com/bintang/usermanagement/exception/ResourceNotFoundException.java
@@ -1,9 +1,20 @@
 package com.bintang.usermanagement.exception;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
 public class ResourceNotFoundException extends RuntimeException{
 
-    public ResourceNotFoundException(String message){
-        super(message);
+    private final String resourceName;
+    private final String fieldName;
+    private final Object fieldValue;
+
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue){
+        super(String.format("%s with %s '%s' not found", resourceName, fieldName, fieldValue));
+        this.resourceName = resourceName;
+        this.fieldName = fieldName;
+        this.fieldValue = fieldValue;
     }
 
 }
