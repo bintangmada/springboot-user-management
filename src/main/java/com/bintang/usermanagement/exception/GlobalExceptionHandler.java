9a69876bda7b2639eccb57dc85882b1c740c6e64
Bintang Suharsono <bintang.mada.suharsono@bni.co.id>
2026-02-02 09:36:01 +0700

    refactor: add anotation @ExceptionHandler to MethodArgumentNotValidException.class

--- src/main/java/com/bintang/usermanagement/exception/GlobalExceptionHandler.java
+++ src/main/java/com/bintang/usermanagement/exception/GlobalExceptionHandler.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.util.Map;
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
+    @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleMethodArgumentNotValid(
             MethodArgumentNotValidException ex,
             HttpServletRequest request
